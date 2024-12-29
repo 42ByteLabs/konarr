b12575a23e8265677a3f6832f7bcb25c02bc8159
GeekMasher <geekmasher@gmail.com>
2024-12-29 18:53:22 +0000

    feat(models): Update dependency models

--- src/models/dependencies/snapshots/mod.rs
+++ src/models/dependencies/snapshots/mod.rs
@@ -5,11 +5,8 @@ use std::{collections::HashMap, str::FromStr};
 use chrono::{DateTime, Utc};
 use geekorm::prelude::*;
 use log::{debug, info};
-use metadata::SnapshotMetadataKey;
 use serde::{Deserialize, Serialize};
 
-#[cfg(feature = "tools-grypedb")]
-use crate::utils::grypedb::GrypeVulnerability;
 use crate::{
     bom::BillOfMaterials,
     models::{security::SecuritySeverity, Alerts, Dependencies, ServerSettings},
@@ -18,7 +15,7 @@ use crate::{
 
 pub mod metadata;
 
-pub use metadata::SnapshotMetadata;
+pub use metadata::{SnapshotMetadata, SnapshotMetadataKey};
 
 /// HashMap of Alerts Summary
 pub type AlertsSummary = HashMap<SecuritySeverity, u16>;
@@ -331,196 +328,6 @@ impl Snapshot {
         Ok(alerts)
     }
 
-    /// Fetch Grype Results for the Snapshot
-    #[cfg(feature = "tools-grypedb")]
-    pub async fn scan_with_grype<'a, T>(
-        &mut self,
-        connection: &'a T,
-        grypedb_connection: &libsql::Connection,
-    ) -> Result<Vec<Alerts>, crate::KonarrError>
-    where
-        T: GeekConnection<Connection = T> + 'a,
-    {
-        use crate::{
-            models::security::{Advisories, AdvisorySource, SecuritySeverity},
-            utils::grypedb::GrypeVulnerabilityMetadata,
-        };
-
-        let mut results = Vec::new();
-
-        if self.components.is_empty() {
-            debug!("Fetching components as none are present");
-
-            self.components = Dependencies::fetch_by_snapshot_id(connection, self.id).await?;
-            for comp in self.components.iter_mut() {
-                comp.fetch(connection).await?;
-            }
-        }
-        debug!("Dependencies Count: {}", self.components.len());
-
-        // Summary of the Security Alerts (cached)
-        let mut summary: AlertsSummary = HashMap::new();
-
-        for dependency in self.components.iter_mut() {
-            log::debug!(
-                "Scanning Dependency: {}",
-                dependency.component_id.data.purl()
-            );
-            let vulns = GrypeVulnerability::find_vulnerabilities(
-                grypedb_connection,
-                &dependency.component_id.data,
-                &dependency.component_version_id.data,
-            )
-            .await?;
-            log::debug!(
-                "Grype Results for {}@{} :: {}",
-                dependency.component_id.data.purl(),
-                dependency.component_version_id.data.version,
-                vulns.len()
-            );
-
-            for vuln in &vulns {
-                debug!("Vulnerability: {:?}", vuln);
-
-                let vuln_metadata: Option<GrypeVulnerabilityMetadata> =
-                    if vuln.id.starts_with("CVE-") {
-                        // Look for the metadata in GrypeDB based on ID + namespace
-                        GrypeVulnerabilityMetadata::query_first(
-                            grypedb_connection,
-                            GrypeVulnerabilityMetadata::query_select()
-                                .where_eq("id", &vuln.id)
-                                .and()
-                                // TODO: Support multiple namespaces
-                                .where_eq("namespace", "nvd:cpe")
-                                .build()?,
-                        )
-                        .await
-                        .ok()
-                    } else if vuln.id.starts_with("GHSA-") {
-                        GrypeVulnerabilityMetadata::query_first(
-                            grypedb_connection,
-                            GrypeVulnerabilityMetadata::query_select()
-                                .where_eq("id", &vuln.id)
-                                .and()
-                                .where_like("namespace", format!("{}%", "github:"))
-                                .build()?,
-                        )
-                        .await
-                        .ok()
-                    } else {
-                        debug!("Skipping non-supported VULN IDs");
-                        continue;
-                    };
-
-                let mut severity = SecuritySeverity::Unknown;
-
-                let mut advisory = if let Some(vuln_metadata) = vuln_metadata {
-                    debug!("Vulnerability Metadata: {:?}", vuln_metadata);
-                    severity = SecuritySeverity::from(vuln_metadata.severity.clone());
-                    let source = vuln_metadata.source();
-
-                    // Advisory
-                    let mut advisory = Advisories::new(vuln.id.clone(), source, severity.clone());
-                    debug!("Advisory: {:?}", advisory);
-                    advisory.fetch_or_create(connection).await?;
-                    advisory.fetch_metadata(connection).await?;
-
-                    // Description
-                    if advisory
-                        .get_metadata(connection, "description")
-                        .await?
-                        .is_none()
-                    {
-                        if !vuln_metadata.description.is_empty() {
-                            advisory
-                                .add_metadata(
-                                    connection,
-                                    "description",
-                                    vuln_metadata.description.clone(),
-                                )
-                                .await?;
-                        }
-                    }
-                    if advisory
-                        .get_metadata(connection, "description")
-                        .await?
-                        .is_none()
-                    {
-                        if !vuln_metadata.description.is_empty() {
-                            advisory
-                                .add_metadata(
-                                    connection,
-                                    "description",
-                                    vuln_metadata.description.clone(),
-                                )
-                                .await?;
-                        }
-                    }
-                    if let Some(cvss) = vuln_metadata.cvss {
-                        advisory
-                            .add_metadata(connection, "cvss", cvss.to_string())
-                            .await?;
-                    }
-                    if let Some(link) = vuln_metadata.urls {
-                        advisory.add_metadata(connection, "urls", link).await?;
-                    } else {
-                        match advisory.source {
-                            AdvisorySource::NationalVulnerabilityDatabase => {
-                                advisory
-                                    .add_metadata(
-                                        connection,
-                                        "urls",
-                                        format!(
-                                            "https://nvd.nist.gov/vuln/detail/{}",
-                                            vuln_metadata.id
-                                        ),
-                                    )
-                                    .await?
-                            }
-                            AdvisorySource::GitHubAdvisoryDatabase => {
-                                advisory
-                                    .add_metadata(
-                                        connection,
-                                        "urls",
-                                        format!(
-                                            "https://github.com/advisories/{}",
-                                            vuln_metadata.id
-                                        ),
-                                    )
-                                    .await?
-                            }
-                            _ => {}
-                        }
-                    }
-
-                    advisory
-                } else {
-                    debug!("No metadata found for vulnerability, creating generic advisory");
-                    let mut advisory =
-                        Advisories::new(vuln.id.clone(), AdvisorySource::Anchore, severity.clone());
-                    advisory.fetch_or_create(connection).await?;
-
-                    advisory
-                };
-                advisory
-                    .add_metadata(connection, "data.source", "GrypeDB".to_string())
-                    .await?;
-
-                let mut alert = Alerts::new(vuln.id.clone(), self.id, dependency.id, advisory.id);
-                alert.find_or_create(connection).await?;
-                debug!("Created Alert: {}", alert.id);
-
-                *summary.entry(severity).or_insert(0) += 1;
-
-                results.push(alert);
-            }
-        }
-
-        self.calculate_alerts(connection, &summary).await?;
-
-        Ok(results)
-    }
-
     /// Calculate a Summary of the Alerts and store in Metadata
     pub async fn calculate_alerts_summary<'a, T>(
         &mut self,
@@ -532,6 +339,8 @@ impl Snapshot {
         let mut summary: HashMap<SecuritySeverity, u16> = HashMap::new();
 
         let mut alerts = Alerts::fetch_by_snapshot_id(connection, self.id).await?;
+        log::debug!("Calculating Alert Summary for {} Alerts", alerts.len());
+
         for alert in alerts.iter_mut() {
             let advisory = alert.fetch_advisory_id(connection).await?;
             let severity = advisory.severity.clone();
